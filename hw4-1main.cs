3a2d84614b457bd1f9c83d20c19b0e86ab63d04d
Elizabeth308 <153708638+Elizabeth308@users.noreply.github.com>
2024-03-31 23:04:58 +0300

    Add files via upload

---
+++ hw4-1main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Prog{
+    static void Main()
+    {
+        OneDimensionMassive<int> intArray = new();
+        OneDimensionMassive<double> doubleArray = new();
+        Console.WriteLine("Enter the length of the array");
+        int leng = int.Parse(Console.ReadLine());
+        for(int i = 0; i < leng; i++)
+        {
+            Console.WriteLine("Enter the element");
+            intArray.AddElement(int.Parse(Console.ReadLine()));
+        }
+        intArray.Print();
+        Console.WriteLine("Enter the length of the array");
+        leng = int.Parse(Console.ReadLine());
+        for(int i = 0; i < leng; i++)
+        {
+            Console.WriteLine("Enter the element");
+            doubleArray.AddElement(double.Parse(Console.ReadLine()));
+        }
+        doubleArray.Print();
+        string command = Console.ReadLine();
+        switch(command)
+        {
+            case "Delete":
+                Console.WriteLine("Enter the index of the element you want to delete");
+                intArray.DeleteElement(int.Parse(Console.ReadLine()));
+                intArray.Print();
+                Console.WriteLine("Enter the index of the element you want to delete");
+                doubleArray.DeleteElement(int.Parse(Console.ReadLine()));
+                doubleArray.Print();
+                break;
+            case "Sort":
+                intArray.SortArray();
+                intArray.Print();
+                doubleArray.SortArray();
+                doubleArray.Print();
+                break;
+            case "Maximum":
+                Console.WriteLine("Max: " + intArray.Maximum());
+                Console.WriteLine("Max: " + doubleArray.Maximum());
+                break;
+            case "Minimum":
+                Console.WriteLine("Min: " + intArray.Minimum());
+                Console.WriteLine("Min: " + doubleArray.Minimum());
+                break;
+            default:
+                Console.WriteLine("Unknown command");
+                break;
+        }
+    }
+}
---
+++ hw4-1program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+
+public sealed class OneDimensionMassive<T>
+{
+    private T[] array;
+    public int Length 
+    {
+        get
+        {
+            return _size;
+        } 
+        set
+        {
+
+        } 
+    }
+    private Random random = new Random();
+    private int _size;
+    private int _capacity;
+
+    public OneDimensionMassive(int capacity) 
+    {
+        array = new T[capacity];
+        _capacity = capacity;
+        _size = 0;
+    }
+
+    public OneDimensionMassive():this(7)
+    {
+
+    }
+
+    public void AddElement(T element)
+    {
+        if(_size >= _capacity)
+        {
+            _capacity = 2 * _capacity + 1;
+            T[] newarray = new T[_capacity];
+            Array.Copy(array, newarray, _size);
+            array = newarray;
+        }
+        array[_size] = element;
+        _size++;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Array");
+        for (int i = 0; i < _size; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void DeleteElement(int indexofelement)
+    {
+        if(indexofelement <= _size)
+        {
+            T[] newarray = new T[_capacity];
+            for (int i = 0; i < _size; i++)
+            {
+                if (i < indexofelement)
+                {
+                    newarray[i] = array[i];
+                }
+                else if (i > indexofelement)
+                {
+                    newarray[i-1] = array[i];
+                }
+            }
+            _size--;
+            array = newarray;
+        }
+        else
+        {
+            Console.WriteLine("Out of range");
+        }
+    }
+
+    public void SortArray()
+    {
+        Array.Sort(array, 0, _size);
+    }
+
+    public bool IfOneElement(Func<T, bool> action)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (action(array[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool IfAllElements(Func<T, bool> action)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (!action(array[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void DoToAllElements(Func<T, T> action)
+    {
+        for(int i = 0; i < _size; i++)
+        {
+            array[i] = action(array[i]);
+        }
+    }
+
+    public T Minimum()
+    {
+        T[] newarray = new T[_size];
+        for (int i = 0; i < _size; i++)
+        {
+            newarray[i] = array[i];
+        }
+        Array.Sort(newarray);
+        return newarray[0];
+    }
+
+    public T Maximum()
+    {
+        T[] newarray = new T[_size];
+        for (int i = 0; i < _size; i++)
+        {
+            newarray[i] = array[i];
+        }
+        Array.Sort(newarray);
+        return newarray[_size-1];
+    }
+
+    public void Reverse()
+    {
+        T[] newarray = new T[_size];
+        int leng = 0;
+        for (int i = _size; i > 0; i--)
+        {
+            newarray[leng] = array[i];
+            leng++;
+        }
+        for (int i = 0; i < _size; i++)
+        {
+            array[i] = newarray[i];
+        }
+    }
+
+    public T[] ReturnAllAppropriate(Func<T, bool> action)
+    {
+        T[] newarray = new T[CountAppropriate(action)];
+        int c = 0;
+        for (int i = 0; i < _size; i++)
+        {
+            if (action(array[i]))
+            {
+                newarray[c] = array[i];
+                c++;
+            }
+        }
+        return newarray;
+    }
+
+    public T ReturnFirstAppropriate (Func<T, bool> action)
+    {
+        for (int i = 0; i < _size; i++)
+        {
+            if (action(array[i]))
+            {
+                return array[i];
+            }
+        }
+        return default(T);
+    }
+
+    public bool DoesElementExist(T element)
+    {
+        if(array.Contains(element))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int CountAppropriate(Func<T, bool> action)
+    {
+        int count = 0;
+        for(int i = 0; i < _size; i++)
+        {
+            if(action(array[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
